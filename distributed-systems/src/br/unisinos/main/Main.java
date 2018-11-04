0dcb139927cfa8b7a94ac72c4c87753dd8726124
brunoschubert <brunoschubertm@gmail.com>
2018-11-03 23:07:50 -0300

    Added base TODO list.

--- distributed-systems/src/br/unisinos/main/Main.java
+++ distributed-systems/src/br/unisinos/main/Main.java
@@ -3,6 +3,13 @@ package br.unisinos.main;
 public class Main {
 
 	public static void main(String[] args) {
+		/*
+		 * TODO LIST
+		 * Window
+		 * Text Editor
+		 * Text Saver
+		 * Text Reader
+		 * ZMQ pub/sub pattern
+		 */
 	}
-
 }
