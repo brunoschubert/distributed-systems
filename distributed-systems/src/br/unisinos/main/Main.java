2e9e3918b830e7eb331d3e7daa1d4d8babcce7f2
brunoschubert <brunoschubertm@gmail.com>
2018-11-04 01:23:50 -0200

    Added Main 0MQ Pipeline Pattern for file Transfer.

--- distributed-systems/src/br/unisinos/main/Main.java
+++ distributed-systems/src/br/unisinos/main/Main.java
@@ -1,18 +1,38 @@
 package br.unisinos.main;
+
+import org.zeromq.*;
+import org.zeromq.ZMQ.Socket;
+import zmq.ZError;
+
+import br.unisinos.server.Server;
+import br.unisinos.client.Client;
 import br.unisinos.editor.TextEditor;
 
 public class Main {
+	private static final int CHUNK_SIZE = 250000;
 
+	//  The main task starts the client and server threads; it's easier
+	//  to test this as a single process with threads, than as multiple
+	//  processes:
 	public static void main(String[] args) {
-		/*
-		 * TODO LIST
-		 * Window
-		 * Text Editor
-		 * Text Saver
-		 * Text Reader
-		 * ZMQ pub/sub pattern
-		 */
-		
-	        TextEditor edit = new TextEditor(); 	
+		ZContext ctx = new ZContext();
+		//  Start child threads
+		ZThread.fork(ctx, new Server());
+		Socket client = ZThread.fork(ctx, new Client());
+		//  Loop until client tells us it's done
+		client.recvStr();
+		//  Kill server thread
+		ctx.destroy();
 	}
+	/*
+	 * TODO LIST
+	 * Text Editor -
+	 * Text Saver  -
+	 * Text Reader -
+	 * ZMQ Test
+	 * ZMQ Pipeline Pattern Test
+	 * ZMQ pub/sub Pattern Test
+	 *
+	 *   TextEditor edit = new TextEditor(); 	
+	 */
 }
