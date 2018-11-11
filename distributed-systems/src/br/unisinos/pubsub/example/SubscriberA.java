55f99399ededa1a79ec6f407b8994e42792d8a95
brunoschubert <brunoschubertm@gmail.com>
2018-11-11 00:23:37 -0200

    Updated Active Subscribers for receiving and processing new classes

--- distributed-systems/src/br/unisinos/pubsub/example/SubscriberA.java
+++ distributed-systems/src/br/unisinos/pubsub/example/SubscriberA.java
@@ -1,27 +1,41 @@
 package br.unisinos.pubsub.example;
 
+import java.util.Random;
+
 import org.zeromq.ZMQ;
 import org.zeromq.ZMQ.Context;
 import org.zeromq.ZMQ.Socket;
 
-public class SubscriberA {
+public class SubscriberA extends Subscriber{
 
-    public static void main (String[] args) {
+	public static void main (String[] args) {
 
-        // Contexto e Subscriber
-        Context context = ZMQ.context(1);
-        Socket subscriber = context.socket(ZMQ.SUB);
+		// Contexto e Subscriber
+		Context context = ZMQ.context(1);
+		Socket subscriber = context.socket(ZMQ.SUB);
+		//Classes Úteis
+		SubscriberA subs = new SubscriberA();
+		//Sorteia Variável na qual a Thread será interrompida
+		Random random = new Random();
+		int stopper = random.nextInt(100);
+		//------------------------------------------------------------------------------------
+		subscriber.connect("tcp://localhost:5563");
+		subscriber.subscribe("Dormir".getBytes());
 
-        subscriber.connect("tcp://localhost:5563");
-        subscriber.subscribe("Msg1".getBytes());
-        while (!Thread.currentThread ().isInterrupted ()) {
-            // Lê envelope com o endereço
-            String address = subscriber.recvStr ();
-            // Lê a mensagem
-            String contents = subscriber.recvStr ();
-            System.out.println(address + " : " + contents);
-        }
-        subscriber.close ();
-        context.term ();
-    }
+		while (subs.isExecuting()) {
+			// Lê envelope com o endereço
+			String address = subscriber.recvStr();
+			// Lê a mensagem
+			String contents = subscriber.recvStr();
+			//passa o conteudo e o numero de parada
+			if(subs.isTimeToStop(contents,stopper)){
+				System.out.println("Fugiu no " + contents);
+				subs.stopExecuting();
+			} else {
+				System.out.println(address + " : " + contents);
+			}
+		} 
+		subscriber.close();
+		context.term();
+	}
 }
--- distributed-systems/src/br/unisinos/pubsub/example/SubscriberB.java
+++ distributed-systems/src/br/unisinos/pubsub/example/SubscriberB.java
@@ -1,26 +1,39 @@
 package br.unisinos.pubsub.example;
 
+import java.util.Random;
+
 import org.zeromq.ZMQ;
 import org.zeromq.ZMQ.Context;
 import org.zeromq.ZMQ.Socket;
 
-public class SubscriberB {
+public class SubscriberB extends Subscriber{
 
-    public static void main (String[] args) {
-        // Contexto e Subscriber
-        Context context = ZMQ.context(1);
-        Socket subscriber = context.socket(ZMQ.SUB);
-
-        subscriber.connect("tcp://localhost:5563");
-        subscriber.subscribe("Msg2".getBytes());
-        while (!Thread.currentThread ().isInterrupted ()) {
-            // Lê envelope com o endereço
-            String address = subscriber.recvStr ();
-            // Lê a mensagem
-            String contents = subscriber.recvStr ();
-            System.out.println(address + " : " + contents);
-        }
-        subscriber.close ();
-        context.term ();
-    }
+	public static void main(String[] args) {
+		// Contexto e Subscriber
+		Context context = ZMQ.context(1);
+		Socket subscriber = context.socket(ZMQ.SUB);
+		//Classes Úteis
+		SubscriberB subs = new SubscriberB();
+		//Sorteia Variável na qual a Thread será interrompida
+		Random random = new Random();
+		int stopper = random.nextInt(100);
+//------------------------------------------------------------------------------------
+		subscriber.connect("tcp://localhost:5563");
+		subscriber.subscribe("Correr".getBytes());
+		while (subs.isExecuting()) {
+			// Lê envelope com o endereço
+			String address = subscriber.recvStr();
+			// Lê a mensagem
+			String contents = subscriber.recvStr();
+			//passa o conteudo e o numero de parada
+			if(subs.isTimeToStop(contents, stopper)){
+				System.out.println("Dormiu na " + contents);
+				subs.stopExecuting();
+			} else {
+				System.out.println(address + " : " + contents);
+			}
+		}
+		subscriber.close();
+		context.term();
+	}
 }
