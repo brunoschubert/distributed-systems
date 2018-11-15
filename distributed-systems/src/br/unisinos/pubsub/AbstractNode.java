fedd16b5791e9c7ecf6abf7b1965eb1ef1e93c91
brunoschubert <brunoschubertm@gmail.com>
2018-11-15 13:41:05 -0200

    Add Abstract Class for PubSub

---
+++ distributed-systems/src/br/unisinos/pubsub/AbstractNode.java
@@ -0,0 +1,15 @@
+package br.unisinos.pubsub;
+
+import org.zeromq.ZMQ.Context;
+import org.zeromq.ZMQ.Socket;
+
+public abstract class AbstractNode {
+	Context pubContext;
+	Socket publisher;
+	
+	Context subContext;
+	Socket subscriber;
+	
+//----------------------------Utility Methods--------------------------------------
+	public 
+}
