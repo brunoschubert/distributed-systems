96a84b8f69b1dd7bf467a604446b3cbb1596a5ae
brunoschubert <brunoschubertm@gmail.com>
2018-11-15 12:25:30 -0200

    Add message class with JSON support

---
+++ distributed-systems/src/br/unisinos/file/message/Message.java
@@ -0,0 +1,63 @@
+package br.unisinos.file.message;
+
+import java.time.Instant;
+
+public class Message {
+	
+	private String nodeId;
+	private String msgId;
+	private String timeUTC;
+	private String msgContents;
+	
+	public Message() {}
+	/*
+	public Message(String nodeId, String msgId, String msgContents){
+		this.nodeId = nodeId;
+		this.msgId = msgId;
+		Instant instant = Instant.now() ;
+		this.timeUTC = instant.toString() ;
+		this.msgContents = msgContents;
+	}*/
+	
+	public String getNodeId() {
+		return nodeId;
+	}
+	public void setNodeId(String nodeId) {
+		this.nodeId = nodeId;
+	}
+	
+	public String getMsgId() {
+		return msgId;
+	}
+	public void setMsgId(String msgId) {
+		this.msgId = msgId;
+	}
+	
+	public String getTimeUTC() {
+		return timeUTC;
+	}
+	public void setTimeUTC() {
+		//get local UTC timestamp
+		Instant instant = Instant.now() ;
+		String timeUTC = instant.toString() ;
+		
+		this.timeUTC = timeUTC;
+	}
+	
+	public String getMsgContents() {
+		return msgContents;
+	}
+	public void setMsgContents(String msgContents) {
+		this.msgContents = msgContents;
+	}
+
+	@Override
+	public String toString() {
+        return "Message{" +
+                "nodeId='" + nodeId + '\'' +
+                ", msgId=" + msgId +
+                ", timeUTC=" + timeUTC +
+                ", msgContents=" + msgContents +
+                '}';
+    }
+}
