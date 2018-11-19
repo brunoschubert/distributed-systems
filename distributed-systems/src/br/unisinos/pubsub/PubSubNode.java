7176b149bf2c10b6ed35241919eeda54980830d4
brunoschubert <brunoschubertm@gmail.com>
2018-11-19 11:05:57 -0200

    Add Listening Method

--- distributed-systems/src/br/unisinos/pubsub/PubSubNode.java
+++ distributed-systems/src/br/unisinos/pubsub/PubSubNode.java
@@ -9,11 +9,6 @@ public class PubSubNode extends AbstractNode {
 			int contextSub, String adressSub, String topic) {
 		//TODO Better Id methods
 		this.nodeId = nodeId;
-		//Starts pub module
-		this.pubContext = ZMQ.context(contextPub);
-		this.publisher = pubContext.socket(ZMQ.PUB);
-		//Binds publishing adress
-		this.publisher.bind(adressPub);
 		//Starts sub module
 		this.subContext = ZMQ.context(contextSub);
 		this.subscriber = subContext.socket(ZMQ.SUB);
@@ -21,7 +16,11 @@ public class PubSubNode extends AbstractNode {
 		this.subscriber.connect(adressSub);
 		//Chose topic to listen to
 		this.subscriber.subscribe(topic.getBytes());
-		
+		//Starts pub module
+		this.pubContext = ZMQ.context(contextPub);
+		this.publisher = pubContext.socket(ZMQ.PUB);
+		//Binds publishing adress
+		this.publisher.bind(adressPub);
 	}
 	public void stopPubSub() {
 		this.publisher.close();
