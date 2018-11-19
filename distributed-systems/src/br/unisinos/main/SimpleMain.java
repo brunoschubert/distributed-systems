78c6cf9404eeabc2c8184af3c5827e74257ab627
brunoschubert <brunoschubertm@gmail.com>
2018-11-19 11:05:16 -0200

    Add Simple PUBSUB example

--- distributed-systems/src/br/unisinos/main/SimpleMain.java
+++ distributed-systems/src/br/unisinos/main/SimpleMain.java
@@ -8,18 +8,18 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 
 import br.unisinos.pubsub.*;
 
-public class Main {
+public class SimpleMain {
 
 	public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException {
 		PubNode pub = new PubNode();
-		pub.startPublisher(1, 1, "tcp://*:5563");
-		
+		pub.startPublisher(1, 1, "tcp://127.0.0.1:5563");
+
 		SubNode sub = new SubNode();
 		Scanner input = new Scanner(System.in);
 		System.out.println("Digite o Tópico para se Inscrever: ");
 		String topic = input.nextLine();
 		
-		sub.startSub(2, 2, "tcp://*:5563", topic);
+		sub.startSub(2, 2, "tcp://127.0.0.1:5563", topic);
 		
 		pub.publishMsg();
 		sub.receiveMessage();
